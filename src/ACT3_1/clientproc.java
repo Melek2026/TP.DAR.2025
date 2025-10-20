3c07fb5d76e6068e3356ade67acc7f9fefdd8fd9
unknown <melek445566778899@gmail.com>
2025-10-20 16:47:54 +0100

    Initial commit

---
+++ src/ACT3_1/clientproc.java
@@ -0,0 +1,15 @@
+package ACT3_1;
+import java.net.Socket;
+public class clientproc extends Thread{
+    Socket s;
+    public static int nbclient;
+    public clientproc(Socket s){
+        nbclient++;
+        this.s=s;
+    }
+    public void run(){
+        System.out.println("je suis client numéro:"+clientproc.nbclient);
+
+    }
+
+}
---
+++ src/ACT3_1/serveur.java
@@ -0,0 +1,27 @@
+package ACT3_1;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class serveur {
+    public static void main(String[] args) throws IOException {
+        //la création de la socket servuer
+        ServerSocket sr=new ServerSocket(1234);
+        System.out.println("le servuer commence a accepter des clients...");
+        //le serveurMultithread commence a accepte beaucoup de clients a la fois
+        while(true){
+            Socket s=sr.accept();
+            System.out.println(s.getRemoteSocketAddress());
+            clientproc p=new clientproc(s);
+            p.start();
+            //ouvrir le flux de communication
+            OutputStream o=s.getOutputStream();
+            //ouvrir le flux de de triement
+            o.write(clientproc.nbclient);
+
+        }
+
+    }
+}
